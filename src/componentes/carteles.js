82d9d73c0b678816cef2f06af3c858adc1921b90
gustavo2321719 <gustavo2321719@gmail.com>
2023-06-07 12:37:52 -0500

    primer commit

---
+++ src/componentes/carteles.js
@@ -0,0 +1,14 @@
+import React from "react"
+function Carteles(props) {
+  return (
+    <div className="paneles">
+      <h2>panel {props.numero}</h2>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, reiciendis magnam! Consectetur numquam dolores aperiam accusantium placeat tempora, necessitatibus quas quam, veritatis culpa fuga hic cupiditate fugit quidem tempore exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, reiciendis magnam! Consectetur numquam dolores aperiam accusantium placeat tempora, necessitatibus quas quam, veritatis culpa fuga hic cupiditate fugit quidem tempore exercitationem.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Carteles;
