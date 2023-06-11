303ca9e46cf7de4251db1bc534547e05d0b6993a
gustavo2321719 <gustavo2321719@gmail.com>
2023-06-11 09:32:04 -0500

    d

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -2,35 +2,35 @@ import React from "react"
 import "./header.css"
 function Header() {
 
-    const toggleBtnIcon = document.querySelector(".toggle_btn i");
-    const dropDownMenu = document.querySelector(".dropdown_menu");
+    const var1 = document.querySelector(".toggle_btn i");
+    const var2 = document.querySelector(".dropdown_menu");
     function onClickBoton() {
-        dropDownMenu.classList.toggle("open");
-        const isOpen = dropDownMenu.classList.contains("open");
+        var2.classList.toggle("open");
+        const isOpe = var2.classList.contains("open");
 
-        toggleBtnIcon.classList = isOpen
+        var1.classList = isOpe
             ? "fa-solid fa-xmark"
             : "fa-solid fa-bars"
-    }
+    };
     return (
         <header>
-            <div className="navbar">
-                <div className="logo"><a href="#">titulo</a></div>
-                <ul className="link">
+            <div className={"navbar"}>
+                <div className={"logo"}><a href="#">titulo</a></div>
+                <ul className={"link"}>
                     <li><a href="inicio">inicio</a></li>
                     <li><a href="redes sociales">redes sociales</a></li>
                     <li><a href="ayuda">ayuda</a></li>
                 </ul>
-                <a href="#" className="action_btn">comprar</a>
-                <div className="toggle_btn" onClick={onClickBoton}>
-                    <i className="fa-solid fa-bars"></i>
+                <a href="#" className={"action_btn"}>comprar</a>
+                <div className={"toggle_btn"} onClick={onClickBoton}>
+                    <i className={"fa-solid fa-bars"}></i>
                 </div>
             </div>
-            <div className="dropdown_menu">
+            <div className={"dropdown_menu"}>
                 <li><a href="inicio">inicio</a></li>
                 <li><a href="redes sociales">redes sociales</a></li>
                 <li><a href="ayuda">ayuda</a></li>
-                <li><a href="#" className="action_btn">comprar</a></li>
+                <li><a href="#" className={"action_btn"}>comprar</a></li>
             </div>
         </header>
 
